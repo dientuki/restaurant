d754bea50771aabf0a64476ef9cac37428570aed
Juan "dientuki" Farias <dientuki@gmail.com>
2024-10-22 00:34:08 -0300

    update model

--- app/Models/ReservationTable.php
+++ app/Models/ReservationTable.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class ReservationTable extends Model
 {
-    //
+    public $incrementing = false;
+
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
+
+    public function table()
+    {
+        return $this->belongsTo(Table::class);
+    }
 }
