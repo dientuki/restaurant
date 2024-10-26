607af0efbc633e505e36893d4a825fc4b4b2b4a4
Juan "dientuki" Farias <dientuki@gmail.com>
2024-10-26 13:51:43 -0300

    add enum with days of the week

---
+++ app/Enum/DayOfWeek.php
@@ -0,0 +1,12 @@
+<?php
+
+enum DayOfWeek: string
+{
+    case Monday = 'Monday';
+    case Tuesday = 'Tuesday';
+    case Wednesday = 'Wednesday';
+    case Thursday = 'Thursday';
+    case Friday = 'Friday';
+    case Saturday = 'Saturday';
+    case Sunday = 'Sunday';
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -34,6 +34,7 @@ class UserFactory extends Factory
 
     /**
      * Indicate that the model's email address should be unverified.
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function unverified(): static
     {
